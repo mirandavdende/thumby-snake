566a5d440aa854165aeb345708119bdb44a53f1d
Timendus <mail@timendus.com>
2022-05-31 19:44:36 +0200

    Have menu use sprite objects to be a bit more readable

--- Games/Snake/menu.py
+++ Games/Snake/menu.py
@@ -5,7 +5,7 @@ selectText = thumby.Sprite(28, 7, bytearray([70,79,121,49,0,56,124,84,92,88,0,12
 
 # Draw a list type menu with the following structure:
 # [
-#   [ width, height, pointer to function, bitmap ],
+#   [ sprite, pointer to function ],
 #   ...
 # ]
 def listType(menu):
@@ -26,7 +26,7 @@ def listType(menu):
             if selected == len(menu):
                 selected = 0
         if thumby.buttonA.pressed():
-            menu[selected][2]()
+            menu[selected][1]()
         while selected > scrollPos + 2:
             scrollPos += 1
         while selected < scrollPos:
@@ -36,15 +36,18 @@ def drawListTypeMenu(menu, start, end, selected):
     thumby.display.fill(0)
     y = 1
     for i in range(start, end + 1):
+        sprite = menu[i][0]
         if i == selected:
             thumby.display.drawFilledRectangle(0, y - 1, 65, 10, 1)
-            inverted = bytearray(len(menu[i][3]))
-            for j in range(len(inverted)):
-                inverted[j] = menu[i][3][j] ^ 0xFF
-            thumby.display.blit(inverted, 2, y, menu[i][0], menu[i][1], -1, 0, 0)
+            inverted = bytearray(sprite.bitmapByteCount)
+            for j in range(sprite.bitmapByteCount):
+                inverted[j] = sprite.bitmap[j] ^ 0xFF
+            thumby.display.blit(inverted, 2, y, sprite.width, sprite.height, -1, 0, 0)
         else:
-            thumby.display.blit(menu[i][3], 2, y, menu[i][0], menu[i][1], -1, 0, 0)
-        y += menu[i][1] + 2
+            sprite.x = 2
+            sprite.y = y
+            thumby.display.drawSprite(sprite)
+        y += sprite.height + 2
 
     drawScrollBar(selected, len(menu))
     thumby.display.drawSprite(selectText)
@@ -52,9 +55,9 @@ def drawListTypeMenu(menu, start, end, selected):
 
 # Draw a settings type menu with the following structure:
 # [
-#   [ width, height, pointer to current option function, bitmap,
+#   [ sprite, pointer to current option function,
 #     [
-#       [ width, height, pointer to set option function, bitmap ],
+#       [ sprite, pointer to set option function ],
 #       ...
 #     ]
 #   ],
@@ -63,7 +66,7 @@ def drawListTypeMenu(menu, start, end, selected):
 def settingsType(menu):
     selected = 0
     while True:
-        option = menu[selected][2]()
+        option = menu[selected][1]()
         drawSettingsTypeMenu(menu, selected, option)
         waitForNewKeyPress()
         if thumby.buttonB.pressed():
@@ -77,15 +80,23 @@ def settingsType(menu):
             if selected == len(menu):
                 selected = 0
         if thumby.buttonA.pressed():
-            numOptions = len(menu[selected][4])
+            numOptions = len(menu[selected][2])
             nextOption = (option + 1) % numOptions
-            menu[selected][4][nextOption][2]()
+            menu[selected][2][nextOption][1]()
 
 def drawSettingsTypeMenu(menu, selected, option):
     thumby.display.fill(0)
-    thumby.display.blit(menu[selected][3], 2, 1, menu[selected][0], menu[selected][1], -1, 0, 0)
-    currentOption = menu[selected][4][option]
-    thumby.display.blit(currentOption[3], 65 - currentOption[0], 29 - currentOption[1], currentOption[0], currentOption[1], -1, 0, 0)
+
+    setting = menu[selected][0]
+    setting.x = 2
+    setting.y = 1
+    thumby.display.drawSprite(setting)
+
+    value = menu[selected][2][option][0]
+    value.x = 65 - value.width
+    value.y = 29 - value.height
+    thumby.display.drawSprite(value)
+
     drawScrollBar(selected, len(menu))
     thumby.display.drawSprite(selectText)
     thumby.display.update()
@@ -118,6 +129,9 @@ def drawScrollBar(pos, total):
 def startGame():
     print("Starting new game!")
 
+def quitGame():
+    print("Exiting game")
+
 controlSetting = 0
 
 def getControlSetting():
@@ -136,26 +150,29 @@ def selectControlDiagonal():
     global controlSetting
     controlSetting = 2
 
+controlsSprite        = thumby.Sprite(40, 8, bytearray([62,127,65,65,65,0,56,124,68,124,56,0,124,124,4,124,120,0,63,127,68,0,124,124,8,12,0,56,124,68,124,56,0,127,127,0,88,92,116,52]))
+controlDPadSprite     = thumby.Sprite(23, 8, bytearray([127,65,65,65,62,0,16,16,0,252,36,36,24,0,32,84,84,120,0,56,68,68,127]))
+controlSnakeSprite    = thumby.Sprite(48, 17, bytearray([0,0,0,0,0,0,0,0,127,64,64,64,0,56,84,84,24,0,126,5,0,63,68,0,0,0,32,84,84,120,0,124,4,4,120,0,56,68,68,127,0,0,0,126,17,17,17,126,144,168,168,72,0,126,136,0,112,168,168,48,0,112,168,168,48,0,248,16,24,0,0,0,144,168,168,72,0,248,8,8,240,0,64,168,168,240,0,254,32,80,136,0,112,168,168,48,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
+controlDiagonalSprite = thumby.Sprite(57, 17, bytearray([0,0,0,0,0,0,127,64,64,64,0,56,84,84,24,0,126,5,0,63,68,0,0,0,20,20,20,0,0,0,60,64,64,60,0,252,36,36,24,0,112,28,7,0,1,127,0,56,84,84,24,0,126,5,0,63,68,252,34,34,34,252,0,0,0,40,40,40,0,0,0,112,136,136,254,0,112,136,136,112,0,120,128,96,128,120,0,248,8,8,240,0,224,56,14,0,248,16,24,0,250,0,48,72,72,248,0,254,8,8,240,0,126,136,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0]))
+
 def optionsMenu():
     settingsType([
         [
-            40, 8, getControlSetting, bytearray([62,127,65,65,65,0,56,124,68,124,56,0,124,124,4,124,120,0,63,127,68,0,124,124,8,12,0,56,124,68,124,56,0,127,127,0,88,92,116,52]),
+            controlsSprite, getControlSetting,
             [
-                [ 23, 8, selectControlDpad, bytearray([127,65,65,65,62,0,16,16,0,252,36,36,24,0,32,84,84,120,0,56,68,68,127]) ],
-                [ 48, 17, selectControlSnake, bytearray([0,0,0,0,0,0,0,0,127,64,64,64,0,56,84,84,24,0,126,5,0,63,68,0,0,0,32,84,84,120,0,124,4,4,120,0,56,68,68,127,0,0,0,126,17,17,17,126,144,168,168,72,0,126,136,0,112,168,168,48,0,112,168,168,48,0,248,16,24,0,0,0,144,168,168,72,0,248,8,8,240,0,64,168,168,240,0,254,32,80,136,0,112,168,168,48,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) ],
-                [ 57, 17, selectControlDiagonal, bytearray([0,0,0,0,0,0,127,64,64,64,0,56,84,84,24,0,126,5,0,63,68,0,0,0,20,20,20,0,0,0,60,64,64,60,0,252,36,36,24,0,112,28,7,0,1,127,0,56,84,84,24,0,126,5,0,63,68,252,34,34,34,252,0,0,0,40,40,40,0,0,0,112,136,136,254,0,112,136,136,112,0,120,128,96,128,120,0,248,8,8,240,0,224,56,14,0,248,16,24,0,250,0,48,72,72,248,0,254,8,8,240,0,126,136,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0]) ]
+                [ controlDPadSprite,     selectControlDpad     ],
+                [ controlSnakeSprite,    selectControlSnake    ],
+                [ controlDiagonalSprite, selectControlDiagonal ]
             ]
         ]
     ])
 
-def quitGame():
-    print("Exiting game")
+newGameSprite = thumby.Sprite(49, 8, bytearray([127,126,12,24,63,127,0,56,124,84,92,88,0,28,124,96,56,96,124,28,0,0,0,62,127,65,121,120,0,32,116,84,124,120,0,124,124,4,124,124,4,124,120,0,56,124,84,92,88]))
+optionsSprite = thumby.Sprite(36, 8, bytearray([62,127,65,65,127,62,0,252,252,36,60,24,0,63,127,68,0,125,125,0,56,124,68,124,56,0,124,124,4,124,120,0,88,92,116,52]))
+quitSprite    = thumby.Sprite(19,8, bytearray([62,127,65,97,255,190,0,60,124,64,124,124,0,125,125,0,63,127,68]))
 
 listType([
-    # New Game
-    [ 49, 8, startGame, bytearray([127,126,12,24,63,127,0,56,124,84,92,88,0,28,124,96,56,96,124,28,0,0,0,62,127,65,121,120,0,32,116,84,124,120,0,124,124,4,124,124,4,124,120,0,56,124,84,92,88]) ],
-    # Options
-    [ 36, 8, optionsMenu, bytearray([62,127,65,65,127,62,0,252,252,36,60,24,0,63,127,68,0,125,125,0,56,124,68,124,56,0,124,124,4,124,120,0,88,92,116,52]) ],
-    # Quit
-    [ 19, 8, quitGame, bytearray([62,127,65,97,255,190,0,60,124,64,124,124,0,125,125,0,63,127,68]) ]
+    [ newGameSprite, startGame   ],
+    [ optionsSprite, optionsMenu ],
+    [ quitSprite,    quitGame    ]
 ])
