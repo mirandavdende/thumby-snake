bbadf1e33e22dce0e740ec9169991c90269412e1
Miranda van der Ende <miranda.vdende@gmail.com>
2022-06-05 18:22:37 +0200

    Draw snake and make it move

--- Games/Snake/Snake.py
+++ Games/Snake/Snake.py
@@ -1,5 +1,13 @@
 import thumby
 
+MOUTH_CLOSED_RIGHT = const(0)
+MOUTH_OPEN_RIGHT = const(1)
+BODY_HOR_RIGHT = const(2)
+BODY_HOR_LEFT = const(3)
+BODY_VER_UP = const(4)
+BODY_VER_DOWN = const(5)
+TAIL_RIGHT = const(6)
+
 def startGame():
     # Snake image data is here
     snakeImages = bytearray([
@@ -9,17 +17,26 @@ def startGame():
         6,6,5,0,6,8,12,14,15,13,14,8
     ])
     snake = thumby.Sprite(4, 4, snakeImages)
+    startPos = 50 # Start position
+    length = 8 # length of the snake segments
+    thumby.display.setFPS(2) # Update every 1/2 second
 
-    # Update every second
-    thumby.display.setFPS(1)
-
-    while(True):
-        snake.x += 1
-        snake.y += 1
-        thumby.display.fill(0)
+    while True:
+        thumby.display.fill(0) # Clear screen
+        snake.setFrame(MOUTH_CLOSED_RIGHT)
+        snake.x = startPos
+        snake.y = 18
         thumby.display.drawSprite(snake)
-        snake.setFrame(snake.currentFrame+1)
+        snake.setFrame(BODY_HOR_RIGHT)
+        for x in range(length):
+            snake.x -= 4
+            thumby.display.drawSprite(snake)
+        snake.setFrame(TAIL_RIGHT)
+        snake.x -= 4
+        thumby.display.drawSprite(snake)
         thumby.display.update()
+        startPos += 1
+
 
 
 # Uncomment this code to show the title screen and menu before starting the game:
